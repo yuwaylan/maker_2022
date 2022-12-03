37efee7e52a45000019d9be50626ef9d7b8e01f6
yuwaylan <0987431104a@gmail.com>
2022-12-03 17:33:33 +0800

    add 2 new project
    app =>
    led, ledmode

---
+++ apps/led/android/app/src/main/kotlin/com/waylan/maker/ctrlLED/led/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.waylan.maker.ctrlLED.led
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apps/ledmode/android/app/src/main/kotlin/com/waylan/maker/ctrlMODE/ledmode/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.waylan.maker.ctrlMODE.ledmode
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
